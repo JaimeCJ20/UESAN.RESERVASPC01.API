e26ade9bed1a6b2ea8bb7fbeb48a6fdb6d7c7076
Jaime S. Castro Marquina <19100255@ue.edu.pe>
2025-05-09 21:06:35 -0500

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace UESAN.RESERVASPC01.API
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+
+        public string? Summary { get; set; }
+    }
+}
